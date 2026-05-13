933bcac6b0672d09eba492a4155865f97a62e91e
Abicumaran <67930921+Abicumaran@users.noreply.github.com>
2026-05-13 03:34:33 -0400

    Delete run_app.py

--- run_app.py
+++
@@ -1,9 +0,0 @@
-import os, sys, subprocess
-
-def main():
-    here = os.path.dirname(os.path.abspath(__file__))
-    app_path = os.path.join(here, "app.py")
-    subprocess.Popen([sys.executable, "-m", "streamlit", "run", app_path, "--server.headless=true"])
-
-if __name__ == "__main__":
-    main()
